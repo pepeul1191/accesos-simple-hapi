a979f206e3db526efb42d8760359231947a4616b
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-02 20:49:30 -0500

    Vistas EJS integradas

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const Hapi = require('hapi');
 const Path = require('path');
 const Inert = require('inert');
 const HRL = require('hapi-routes-loader');
+const Vision = require('vision');
+const Ejs = require('ejs');
 const server = new Hapi.Server();
 
 var constants = require('./config/constants');
@@ -21,34 +23,52 @@ server.connection({
 
 server.ext('onPreResponse', function(request, reply){
   if (request.response.header) {
-    request.response.header('Server', 'Ubuntu');
+    request.response.header('Server', 'Ubuntu;hapi.js');
+    request.response.header('x-powered-by', 'hapi.js;Node.js');
   }
   reply.continue();
 });
 
 server.on('response', function (request) {
   if (constants.data['ambiente'] == 'desarrollo'){
-    console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.url.path + ' --> ' + request.response.statusCode);
+    console.log(
+    	request.info.remoteAddress + ': ' +
+    	request.method.toUpperCase() + ' ' +
+    	request.url.path + ' --> ' +
+    	request.response.statusCode
+    );
   }
 });
 
 server.register(
   [
     Inert,
+    Vision,
     {
       register: HRL,
       options: {
-        dirname: __dirname, //must be a string with a root path
-        pathRoutes: '/routes'
-      }
+      	dirname: __dirname, //must be a string with a root path
+      	pathRoutes: '/routes'
+    	}
     },
-
-  ], (err) => {
-  server.start((err) => {
-    console.log('Running web app at: ' + server.uri);
-  });
-});
-
+  ],
+  (err) => {
+	 	if (typeof err !== 'undefined') {
+	 		console.log(err);
+		}else{
+			server.views({
+		    engines: {
+		      html: Ejs,
+		    },
+		    path: __dirname + '/views',
+		    layout: false
+		  });
+			server.start((err) => {
+			  console.log('Running HapiJs web app');
+			});
+		}
+	}
+);
 server.route({
   method: 'GET',
   path: '/{param*}',
